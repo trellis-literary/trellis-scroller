0a2968ed6ec802c8b4b7f93e03888282efb50877
Elsa Brown <elsambrown@gmail.com>
2021-09-29 16:18:50 -0400

    adds console.log

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import '../scss/style.scss';
 
 const init = () => {
+    console.log('HELLO SCRIPT BUNDLER');
+
     const newDiv = document.createElement('div');
     newDiv.className = 'script-bundler-div';
     newDiv.innerHTML = "This element was created with script-bundler";
