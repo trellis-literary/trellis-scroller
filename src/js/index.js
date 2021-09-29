1cefad6fb63699af5f2ea63caef07e4b2d3056e3
Elsa Brown <elsambrown@gmail.com>
2021-09-29 15:09:01 -0400

    wip

---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+import '../scss/style.scss';
+
+const buildImageScrollers = () => {
+    console.log('building scrollers');
+    const page = document.getElementById('page');
+    const firstSection = page.querySelector('.page-section');
+
+    const scrollers = document.createElement('div');
+    scrollers.className = 'scrollers';
+
+    const scrollerOne = document.createElement('div');
+    scrollerOne.className = 'scroller';
+    scrollerOne.id = 'scroller-one';
+
+    const scrollerTwo = document.createElement('div');
+    scrollerTwo.className = 'scroller';
+    scrollerTwo.id = 'scroller-two';
+
+    scrollers.appendChild(scrollerOne);
+    scrollers.appendChild(scrollerTwo);
+    firstSection.appendChild(scrollers);
+
+    const footer = document.querySelector('footer');
+    const images = footer.querySelectorAll('img');
+
+    [...images].forEach((image, idx) => {
+        const imgSrc = image.getAttribute("data-src");
+        console.log('images: ', images);
+
+        const newImg = document.createElement('img');
+        newImg.src = imgSrc;
+
+        idx % 2 ? 
+            scrollerOne.appendChild(newImg) :
+            scrollerTwo.appendChild(newImg);
+    });
+}
+
+buildImageScrollers();
+
+// const init = () => {
+//     switch (document.readyState) {
+//         case 'complete':
+//         case 'interactive':
+//             buildImageScrollers();
+//             break;
+//         default:
+//             document.onreadystatechange = () => {
+//                 if ( document.readyState === 'complete' || 
+//                     document.readyState === 'interactive' ) {
+//                     buildImageScrollers();
+//                 }
+//             }
+//             break;
+//     }
+// }
+
+// init();
